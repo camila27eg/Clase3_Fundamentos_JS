4830b7fa674d7f593c073e545a86f8b34e89cf46
camila27eg <celorriaga@centro.edu.mx>
2024-10-07 09:53:00 -0600

    Commit 1

---
+++ Ejercicio1/javascript-1.js
@@ -0,0 +1,23 @@
+const element = document.getElementById("tester");
+let start;
+let offset = 0;
+
+function move(timeStamp) {
+    // timeStamp es un "dato" que nos devuelve a forma de "parámetro" la función request animation frame automáticamente, e indica cuánto tiempo ha pasado desde la primera vez que se cargó el sitio.
+  
+    // "start" lo estamos usando como un marcador para calcular cuánto tiempo ha pasado desde la primera vez que se invocó la función requestAnimationFrame y el momento presente.
+    if (start === undefined) {
+        start = timeStamp;
+    }
+
+    const elapsed = timeStamp - start;
+    offset = Math.min(0.1 * elapsed, 500); // Math.min() es usado aqui para asegurarnos que el elemento se detenga a los 500px
+    element.style.transform = `translateX(${offset}px)`;
+  
+    requestAnimationFrame(move);
+}
+
+requestAnimationFrame(move);
+
+
+// Happy Coding! 👾
